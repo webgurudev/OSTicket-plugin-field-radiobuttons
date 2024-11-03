70ba341edb577fcf1f0fd0645680d2ea4c65dd38
webgurudev <webgurudev@gmail.com>
2024-11-02 22:32:46 -0700

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -1,9 +1,9 @@
 <?php
 return array(
     'id' =>             'field:radiobuttons', # notrans
-    'version' =>        '0.2',
+    'version' =>        '0.3',
     'name' =>           /* trans */ 'Radiobuttons',
-    'author' =>         'Micke1101',
+    'author' =>         'Micke1101, Webgurudev',
     'description' =>    /* trans */ 'Adds the possibility to use radiobuttons in your forms.',
     'url' =>            'https://github.com/Micke1101/OSTicket-plugin-field-radiobuttons',
     'plugin' =>         'radiobuttons.php:RadiobuttonsPlugin'
